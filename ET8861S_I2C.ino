0bce11e6e3a4531d4d2614873e0798c94767ce33
flp <flp@gmx.de>
2021-05-26 00:50:26 +0200

    feat: implements setting of individual segments

--- ET8861S_I2C.ino
+++ ET8861S_I2C.ino
@@ -11,27 +11,34 @@
  * No data, just the command
  */
 #define COMMAND_CLEAR 0x00
+
 /**
  * Followed by 7 bytes character data
  */
 #define COMMAND_SET_STRING 0x01
+
 /**
  * Followed by 1 byte position and 1 byte character data
  */
 #define COMMAND_SET_CHARACTER 0x02
+
 /**
  * Followed by 1 byte symbol data
  */
 #define COMMAND_SET_SYMBOLS 0x03
 
 /**
- * Followed by 1 byte position and 2 bytes segment data,
- * MSB first.
+ * Followed by 1 byte position and 2 bytes segment data
+ * (MSB first).
+ * A position which received segment data is locked for
+ * string/character update. The lock will be removed
+ * if segment data 0 is received for that position.
  */
 #define COMMAND_SET_SEGMENTS 0x04
 
 static uint8_t receiveByte = 0;
 char displayString[8] = "       ";
+uint8_t stringPositionFilter = 0xff;
 static bool isDirty = false;
 
 void receiveEvent(uint8_t bytesInBuffer) {
@@ -102,6 +109,26 @@ void receiveCommandSetSegments() {
   segments <<= 8;
   segments |= TinyWireS.receive();
   ET8861.setSegments(position, segments);
+  if(segments == 0L) {
+    // allow this position again
+    stringPositionFilter |=  (1 << position);    
+  } else {
+    // lock this position
+    stringPositionFilter &=  ~(1 << position);
+  }
+}
+
+void applyFilterToDisplayString() {
+  for(uint8_t position = 0; position < 7; position ++) {
+    if(! (stringPositionFilter & (1 << position))) {
+      displayString[position] = 0;
+      continue;
+    }
+
+    if(displayString[position] == 0) {
+      displayString[position] = ' ';
+    }
+  }
 }
 
 void showGreeting() {
@@ -115,7 +142,9 @@ void showGreeting() {
   sprintf(displayString, "I2C $%X", I2C_ADDRESS);
   ET8861.setString(displayString);
   ET8861.setSegments(2, 0b0000110001001000);
-  delay(3000);
+  delay(1500);
+  sprintf(displayString, "       ");
+  isDirty = true;
 }
 
 void setup() {
@@ -123,7 +152,6 @@ void setup() {
 #if SHOW_GREETING
   showGreeting();
 #endif
-  ET8861.setString("       ");
 
   TinyWireS.begin(I2C_ADDRESS);
   TinyWireS.onReceive(receiveEvent);
@@ -131,6 +159,7 @@ void setup() {
 
 void loop() {
   if (isDirty) {
+    applyFilterToDisplayString();
     ET8861.setString(displayString);
     isDirty = false;
   }
